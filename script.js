bd2b515aaff01e12d708a136e66444a1eef0d609
vijay <vijaysparks21@gmail.com>
2024-05-05 13:02:00 +0530

    few changes added

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
+const log=(param)=>console.log(param)
+
 for(let i=1;i<=10;i++){
     console.log(i)
 }
 console.log(a) 
 var a = 10
 console.log(a)
+// push
+const provisionalstore = ["vijay","aravindh","jackson"]
+      provisionalstore.unshift("andrew")
+console.log(provisionalstore)
+
+
+ const removedperson = provisionalstore.shift()
+ console.log(provisionalstore)
+log(removedperson)
+
+log("hi i am working properly")
